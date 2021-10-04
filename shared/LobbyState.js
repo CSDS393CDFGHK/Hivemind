a3de9bbc97666c9fcae4d1dfc851fdacb49ba83c
Ethan Frank <edf32@case.edu>
2021-10-04 09:50:34 -0400

    Implement LobbyState.js

---
+++ shared/LobbyState.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview This Enum defines the states of the game/lobby. 
+ * Lobby: Players are in the lobby waiting for the game to start
+ * Game: Players are playing the game
+ * GameEnd: The game has ended, players wait for the Lobby Owner to bring them back to the lobby.
+ */
+const MessageType = {
+    LOBBY: "lobby",
+    GAME: "game",
+    GAMEEND: "gameend",
+}
