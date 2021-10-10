2ebe8329e980b8580c49d7f961de95812414baf2
Ethan Frank <edf32@case.edu>
2021-10-10 13:54:21 -0400

    Add LOBBY_STATE to MessageType enum

--- shared/MessageType.js
+++ shared/MessageType.js
@@ -9,6 +9,8 @@ const MessageType = {
     PLAYER_JOIN: "player_join",
     PLAYER_LEAVE: "player_leave",
     WORD: "word",
+    CREATE_LOBBY: "create_lobby",
+    LOBBY_ID: "lobby_id",
     PLAYER_DATA: "player_data",
     NEXT_TURN: "next_turn",
     LOBBY_STATE: "lobby_state",
